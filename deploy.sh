5d3d3e208c2f277509959860ea1a8f918210f41a
liguoqinjim <liguoqinjim23@gmail.com>
2018-10-19 20:05:59 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -7,5 +7,5 @@ cd files2
 cp -R ../files/add_readme.md README.md
 git add -f --ignore-errors --all
 git -c user.name='liguoqinjim' -c user.email='liguoqinjim23@gmail.com' commit -m "deploy by travis"
-git push -f -q https://${token}@github.com/liguoqinjim/github_emoji.git master
+git push -f -q https://${token}@github.com/liguoqinjim/all_readme.git master
 
